ca3cda98a7c54452c1ac7c3a520b5a210d62199d
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-01 01:06:16 -0400

    Add vue-material

---
+++ src/config/vue-material.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import VueMaterial from "vue-material";
+
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+Vue.use(VueMaterial);
