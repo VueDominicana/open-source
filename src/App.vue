067c9cabaaf9cecac583012ee20d312e5736af10
ljcordero <cars098@hotmail.com>
2020-05-03 16:51:42 -0400

    fullscreen loading component added (#17)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <loading :active="isLoading" />
     <AppNav />
     <div class="container">
       <router-view />
@@ -9,10 +10,28 @@
 
 <script>
 import AppNav from "@/components/Nav";
+// Import component
+import Loading from "vue-loading-overlay";
+// Import stylesheet
+import "vue-loading-overlay/dist/vue-loading.css";
+
 export default {
   name: "App",
   components: {
+    Loading,
     AppNav
+  },
+  computed: {
+    isLoading() {
+      return this.$store.getters["App/isLoading"];
+    }
+  },
+  mounted() {
+    //TODO remove sample
+    this.$store.dispatch("App/setLoading", true);
+    setTimeout(() => {
+      this.$store.dispatch("App/setLoading", false);
+    }, 5000);
   }
 };
 </script>
