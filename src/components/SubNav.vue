4c6fb44440cb149537d2a3cb0a97d5a3211b4594
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-03 03:46:32 -0400

    Fix conflicts

---
+++ src/components/SubNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-link v-for="section in sections" :to="{ name: section.name }" :key="section.name">
+      {{ section.label }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubHeader",
+  props: {
+    sections: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/config/materialize.js
@@ -0,0 +1,2 @@
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
