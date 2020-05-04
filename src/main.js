453f8f19ea112b3d2350a1e42f601f0a3ece2deb
Vyzaldy Andrés Sanchez <vyzaldysanchez@gmail.com>
2020-05-03 20:58:41 -0400

    virtual-scroller - Installs and configures virtual scroller library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+// vue-virtual-scroller needs this polyfill: https://github.com/Akryum/vue-virtual-scroller#installation
+import "intersection-observer";
+
 import Vue from "vue";
+import VueVirtualScroller from "vue-virtual-scroller";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+
 import App from "@/App.vue";
 import router from "@/router";
 import "@/config";
 import store from "./store/store";
+
+Vue.use(VueVirtualScroller);
 
 new Vue({
   router,
