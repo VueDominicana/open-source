2d5732ca6618487b6987816d17d45dad07fe6cd7
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-05 09:01:02 -0400

    Add repository card and language component (#22)
    * Add language component
    
    * Add repository card
    
    * Add languages color
    
    * Add helpers hexToRgb and setContrast
    
    * Use repositories card in populars
    
    * Rename helpers.js -> colors.js
    
    * Support shorthand form for hex colors
    
    * Simplify language color generation
    
    * Handle repositories without language
    
    * Get rid of the color ternary
    
    * Add icons within links

--- src/components/RepositoryCard.vue
+++ src/components/RepositoryCard.vue
@@ -1,11 +1,93 @@
 <template>
   <div>
-    Repository Card
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card">
+          <div class="card-content black-text">
+            <header>
+              <strong>#1</strong>
+              <a href="#">Amejia481/Associate-Android-Developer-Certification</a>
+              <div class="star">
+                <i class="material-icons">star</i>
+                <span>723</span>
+              </div>
+            </header>
+            <p>
+              All the info and materials about the certification that I've collected so far
+            </p>
+          </div>
+          <div class="card-action languages">
+            <languages :languages="'javascript c# random'" />
+          </div>
+          <div class="card-action user">
+            <a href="#" class="center-items">
+              <img
+                class="responsive-img circle"
+                width="32"
+                src="https://avatars2.githubusercontent.com/u/773158?v=4&s=64"
+              />
+              <span>ARTURO MEJIA</span>
+            </a>
+            <a href="#" class="center-items">
+              <i class="material-icons">link</i>
+              <span>Github Project</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import languages from "@/components/Languages";
+
+export default {
+  name: "Repository",
+  components: {
+    languages
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  display: flex;
+  font-size: 24px;
+  margin-bottom: 10px;
+  justify-content: center;
+  strong {
+    margin-right: 20px;
+  }
+  a {
+    flex: 1;
+  }
+  .star {
+    display: flex;
+    align-items: center;
+    i {
+      margin-right: 3px;
+    }
+  }
+}
+
+.user {
+  display: flex;
+  align-items: center;
+
+  img,
+  i {
+    margin-right: 10px;
+  }
+
+  a,
+  i {
+    color: #039be5 !important;
+  }
+
+  .center-items {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/util/colors.js
@@ -0,0 +1,33 @@
+// @ts-check
+/** Convert hex color to rgb
+ * @param {string} hex
+ * @returns {(object|null)}
+ * @see https://stackoverflow.com/a/5624139/3367568
+ */
+export function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+    return r + r + g + g + b + b;
+  });
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (result) {
+    return {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    };
+  }
+
+  return null;
+}
+
+/** Set the contrast based on brightness of a rgb color
+ * @param {Object} rgb
+ * @returns {string}
+ * @see https://stackoverflow.com/questions/11867545/change-text-color-based-on-brightness-of-the-covered-background-area#comment80648498_11868159
+ */
+export function setContrast(rgb) {
+  return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000 > 125 ? "black" : "white";
+}
