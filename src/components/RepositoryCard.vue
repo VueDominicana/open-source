f2cc09392fd9db407c192d24e09c5e53baf79ae7
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-08 22:39:10 -0400

    Fix conflicts

--- src/components/RepositoryCard.vue
+++ src/components/RepositoryCard.vue
@@ -1,38 +1,36 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col s12 m12">
-        <div class="card">
-          <div class="card-content black-text">
-            <header>
-              <strong>#1</strong>
-              <a href="#">Amejia481/Associate-Android-Developer-Certification</a>
-              <div class="star">
-                <i class="material-icons">star</i>
-                <span>723</span>
-              </div>
-            </header>
-            <p>
-              All the info and materials about the certification that I've collected so far
-            </p>
-          </div>
-          <div class="card-action languages">
-            <languages :languages="'javascript c# random'" />
-          </div>
-          <div class="card-action user">
-            <a href="#" class="center-items">
-              <img
-                class="responsive-img circle"
-                width="32"
-                src="https://avatars2.githubusercontent.com/u/773158?v=4&s=64"
-              />
-              <span>ARTURO MEJIA</span>
-            </a>
-            <a href="#" class="center-items">
-              <i class="material-icons">link</i>
-              <span>Github Project</span>
-            </a>
-          </div>
+  <div class="row">
+    <div class="col s12 m12">
+      <div class="card">
+        <div class="card-content black-text">
+          <header>
+            <strong>#1</strong>
+            <a href="#">Amejia481/Associate-Android-Developer-Certification</a>
+            <div class="star">
+              <i class="material-icons">star</i>
+              <span>723</span>
+            </div>
+          </header>
+          <p>
+            All the info and materials about the certification that I've collected so far
+          </p>
+        </div>
+        <div class="card-action languages">
+          <languages :languages="'javascript c# random'" />
+        </div>
+        <div class="card-action user">
+          <a href="#" class="center-items">
+            <img
+              class="responsive-img circle"
+              width="32"
+              src="https://avatars2.githubusercontent.com/u/773158?v=4&s=64"
+            />
+            <span>ARTURO MEJIA</span>
+          </a>
+          <a href="#" class="center-items">
+            <i class="material-icons">link</i>
+            <span>Github Project</span>
+          </a>
         </div>
       </div>
     </div>
--- src/views/PopularRepositories.vue
+++ src/views/PopularRepositories.vue
@@ -1,20 +1,59 @@
 <template>
   <div>
     <h3 class="center-align">Popular Repositories</h3>
-    <slot />
-    <Repositories />
+    <p class="center">
+      Showing <strong>{{ repositories.length.toLocaleString() }}</strong> repositories
+      <span>sorted by stars.</span>
+    </p>
+    <InputSearch label="Filter repository by name or description..." v-model="search" />
+    <Repositories v-for="repository in filteredRepositories" :repository="repository" :key="repository.id" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import * as JsSearch from "js-search";
+import sortBy from "lodash/sortBy";
 import Repositories from "@/components/RepositoryCard";
+import InputSearch from "@/components/InputSearch";
+const searcher = new JsSearch.Search("id");
+searcher.addIndex("name");
+searcher.addIndex("description");
 
 export default {
   name: "PopularRepositories",
   components: {
-    Repositories
+    Repositories,
+    InputSearch
+  },
+  data() {
+    return {
+      search: ""
+    };
+  },
+  computed: {
+    ...mapGetters({
+      repositories: "About/repositories"
+    }),
+    filteredRepositories() {
+      const results = searcher.search(this.search).slice(0, 10);
+      return sortBy(results, repo => -repo.stargazers);
+    }
+  },
+  watch: {
+    repositories: {
+      handler() {
+        searcher.addDocuments(this.repositories);
+      },
+      immediate: true
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+span {
+  font-style: italic;
+  font-weight: 600;
+}
+</style>
