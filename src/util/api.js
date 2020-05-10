b35e5cb2075f12ddbe9d3e1aca49e88cd69e3796
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-10 00:47:53 -0400

    Sort desc and add position

--- src/util/api.js
+++ src/util/api.js
@@ -1,12 +1,23 @@
 import axios from "axios";
+import sortBy from "lodash/sortBy";
 
 axios.defaults.baseURL = "https://raw.githubusercontent.com/DannyFeliz/opensource/develop/public/data";
 
 export default {
-  getDevelopers() {
-    return axios.get("/users.json").then(({ data }) => data);
+  async getDevelopers() {
+    const { data: developers } = await axios.get("/users.json");
+    const sortedDevelopers = sortBy(developers, dev => -dev.followers);
+    return sortedDevelopers.map((dev, position) => {
+      dev.position = position + 1;
+      return dev;
+    });
   },
-  getRepositories() {
-    return axios.get("/repos.json").then(({ data }) => data);
+  async getRepositories() {
+    const { data: repositories } = await axios.get("/repos.json");
+    const sortedRepositories = sortBy(repositories, repo => -repo.stargazers);
+    return sortedRepositories.map((repo, position) => {
+      repo.position = position + 1;
+      return repo;
+    });
   }
 };
