d66bc2793997a67d48163621e568c9b71c099d50
larizzatg <larizzatg@hotmail.com>
2020-05-10 16:26:06 -0400

    add column classes for card responsiveness

--- src/components/DeveloperCard.vue
+++ src/components/DeveloperCard.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="card hoverable">
-    <div class="card-content center-align">
-      <span class="developer__rank">#1</span>
-      <a href="https://github.com/yyx990803" target="_blank">
-        <img
-          src="https://avatars3.githubusercontent.com/u/499550?s=400&u=de41ec9325e8a92e281b96a1514a0fd1cd81ad4a&v=4"
-          alt="evan yue profile image"
-          class="developer__photo circle responsive-img"
-        />
-        <h3 class="card-title developer__name">Evan Yue</h3>
-      </a>
-      <p>Followed by: 585</p>
-    </div>
-    <div class="card-action">
-      <ul class="developer__statistics">
-        <li>0 repositories</li>
-        <li>0 forks</li>
-      </ul>
-    </div>
-    <div class="card-action">
-      <a href="https://github.com/yyx990803" target="_blank" style="text-transform: lowercase;">
-        https://github.com/yyx990803
-      </a>
+  <div class="col s12 m6 l4">
+    <div class="card hoverable">
+      <div class="card-content center-align">
+        <span class="developer__rank">#1</span>
+        <a href="https://github.com/yyx990803" target="_blank">
+          <img
+            src="https://avatars3.githubusercontent.com/u/499550?s=400&u=de41ec9325e8a92e281b96a1514a0fd1cd81ad4a&v=4"
+            alt="evan yue profile image"
+            class="developer__photo circle responsive-img"
+          />
+          <h3 class="card-title developer__name">Evan Yue</h3>
+        </a>
+        <p>Followed by: 585</p>
+      </div>
+      <div class="card-action">
+        <ul class="developer__statistics">
+          <li>0 repositories</li>
+          <li>0 forks</li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <a href="https://github.com/yyx990803" target="_blank" style="text-transform: lowercase;">
+          https://github.com/yyx990803
+        </a>
+      </div>
     </div>
   </div>
 </template>
