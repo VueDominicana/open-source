a6971f72daab6aed653e64ce983abc3ba458c461
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-10 00:49:24 -0400

    Remove validation in input search

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="row input-search">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s12">
-          <i class="material-icons prefix">search</i>
-          <input id="search" type="search" class="validate" @input="onChange" autocomplete="off" />
-          <label for="search">{{ label }}</label>
-        </div>
-      </div>
-    </form>
+    <div class="input-field col s12">
+      <i class="material-icons prefix">search</i>
+      <input id="search" type="search" @input="onChange" autocomplete="off" />
+      <label for="search">{{ label }}</label>
+    </div>
   </div>
 </template>
 
@@ -30,30 +26,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.input-field {
-  label {
-    color: #039be5 !important;
-  }
+.input-search {
+  margin-bottom: 0 !important;
+  .input-field {
+    label {
+      color: #039be5 !important;
+    }
 
-  input {
-    &:focus {
-      + label {
-        color: #039be5 !important;
+    input {
+      &:focus {
+        + label {
+          color: #039be5 !important;
+        }
+
+        border-bottom: 1px solid #039be5 !important;
+        box-shadow: 0 1px 0 0 #039be5 !important;
       }
-
-      border-bottom: 1px solid #039be5 !important;
-      box-shadow: 0 1px 0 0 #039be5 !important;
     }
 
-    &.valid,
-    &.invalid {
-      border-bottom: 1px solid #039be5 !important;
-      box-shadow: 0 1px 0 0 #039be5 !important;
+    .prefix.active {
+      color: #039be5 !important;
     }
-  }
-
-  .prefix.active {
-    color: #039be5 !important;
   }
 }
 </style>
