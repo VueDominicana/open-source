cbe018c1fbc639cd363a4c9753c76ab56151b26d
Danny Feliz <dannyfeliz08@gmail.com>
2020-06-09 11:12:19 -0400

    Remove early return

--- src/composable/useSearchDeveloper.js
+++ src/composable/useSearchDeveloper.js
@@ -6,22 +6,22 @@ const developerSearcher = new Searcher().setField("name").setField("login");
 
 export function useSearchDeveloper() {
   function searchPopular(searchTerm) {
-    const developers = store.state.Developers.developers;
+    let developers = store.state.Developers.developers;
     developerSearcher.setData(developers);
 
     if (!searchTerm) {
-      return developers.slice(0, 10);
+      developers = developers.slice(0, 10);
     }
 
     return sortBy(developerSearcher.findAll(searchTerm), dev => -dev.followers).slice(0, 10);
   }
 
   function searchNewDevelopers(searchTerm) {
-    const developers = store.getters["Developers/newDevelopers"];
+    let developers = store.getters["Developers/newDevelopers"];
     developerSearcher.setData(developers);
 
     if (!searchTerm) {
-      return developers.slice(0, 10);
+      developers = developers.slice(0, 10);
     }
 
     return sortBy(developerSearcher.findAll(searchTerm), dev => -Number(new Date(dev.createdAt))).slice(0, 10);
